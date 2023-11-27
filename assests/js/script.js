1669f986bd09b6f5907049b91b7446226e2694ab
Beatriz Bravo <beatz.bravo@gmail.com>
2023-11-27 13:42:38 -0700

    added function to start button

---
+++ assests/js/script.js
@@ -0,0 +1,9 @@
+var startButton = document.querySelector("#start-button");
+
+function startQuiz(event) {
+  //this is where the timer will start
+  console.log("the quiz has started");
+  //it will display question 1
+}
+
+startButton.addEventListener("click", startQuiz);
