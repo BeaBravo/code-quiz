6fdaaea7bcdaa9afb1065c3914bfa76149099a6b
Beatriz Bravo <beatz.bravo@gmail.com>
2023-11-27 15:47:05 -0700

    when clicked the question, it'll go to the next

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,9 +1,37 @@
 var startButton = document.querySelector("#start-button");
+var startPage = document.querySelector("#start-page");
+var questions = document.querySelectorAll(".Quiz-questions");
+var answered = document.querySelectorAll("#choices");
+var nextQuestionAns = document.querySelector(".user-view");
+var index = 0;
+var currentQuestion;
 
+console.log("display", questions[0].style);
+console.log(answered);
 function startQuiz(event) {
+  event.stopPropagation();
   //this is where the timer will start
-  console.log("the quiz has started");
+  startPage.setAttribute("style", "display:none"); //start page will disappear
+
   //it will display question 1
+  questions[0].setAttribute("style", "display:block");
+}
+
+function nextQuestion(direction) {
+  index = index + direction;
+  if (index == questions.length - 1) {
+    return;
+  }
+  currentQuestion = questions[index];
+  questions[index - 1].setAttribute("style", "display:none");
+  currentQuestion.setAttribute("style", "display:block");
+  console.log("current question is " + (index + 1));
+  console.log("index", index);
 }
 
 startButton.addEventListener("click", startQuiz);
+
+nextQuestionAns.addEventListener("click", function (event) {
+  event.stopPropagation();
+  nextQuestion(1);
+});
