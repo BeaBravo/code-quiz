936fc086d7c974f1249e320da6ec2f57e297c6a6
Beatriz Bravo <beatz.bravo@gmail.com>
2023-11-29 16:33:41 -0700

    started the javascript again

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,37 +1,48 @@
+//Dependencies
 var startButton = document.querySelector("#start-button");
 var startPage = document.querySelector("#start-page");
 var questions = document.querySelectorAll(".Quiz-questions");
+var random = document.getElementById;
 // var answer = querySelector.("")
 var nextQuestionAns = document.querySelector(".user-view");
-var index = 0;
-var currentQuestion;
 
-console.log("display", questions[0].style);
-// console.log(answered);
-function startQuiz(event) {
-  event.stopPropagation();
-  //this is where the timer will start
-  startPage.setAttribute("style", "display:none"); //start page will disappear
+//Global variables
+// questions as objects with the answers
+var question1 = {
+  question: "Arrays in JavaScript can be used to store ___.",
+  option1: "1. numbers and strings",
+  option2: "2. other arrays",
+  option3: "3. booleans",
+  option4: "4. all of the above",
+  correct: "option4",
+};
 
-  //it will display question 1
-  questions[0].setAttribute("style", "display:block");
+var question2 = {
+  question:
+    "Which of the following methods is used to access an HTML element by their id?",
+  option1: '1. querySelector("#idOfElement")',
+  option2: '2. getElementById("idOfElement")',
+  option3: '3. querySelector(".idOfElement")',
+  option4: "4. both 1 and 2 are correct",
+  correct: "option4",
+};
+
+var questions = [question1, question2];
+
+//FUNCTIONS
+function init() {
+  console.log(question1.question);
+  console.log(question1.option1);
+  console.log(questions);
+}
+function startQuiz() {
+  //start timer
+  //go to first question
 }
 
-function nextQuestion(direction) {
-  index = index + direction;
-  if (index == questions.length - 1) {
-    return;
-  }
-  currentQuestion = questions[index];
-  questions[index - 1].setAttribute("style", "display:none");
-  currentQuestion.setAttribute("style", "display:block");
-  console.log("current question is " + (index + 1));
-  console.log("index", index);
-}
+//USER INTERACTIONS
 
 startButton.addEventListener("click", startQuiz);
 
-nextQuestionAns.addEventListener("click", function (event) {
-  event.stopPropagation();
-  nextQuestion(1);
-});
+//INITIALIZATION
+init();
