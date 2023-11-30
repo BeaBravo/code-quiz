b8f01fe72e47befef14eae5be2537e4c7d553773
Beatriz Bravo <beatz.bravo@gmail.com>
2023-11-29 19:04:52 -0700

    created a function to check if answer is right

--- assests/js/script.js
+++ assests/js/script.js
@@ -5,8 +5,11 @@ var gamePage = document.querySelector("#quiz-questions");
 var questionDisplay = document.querySelector("#current-question");
 var choices = document.querySelector("#multiple-choice");
 var option = choices.children;
+var message = document.querySelector("#message");
 
 //Global variables
+var answer;
+var currentQuestion;
 // questions as objects with the answers
 var question1 = {
   question: "Arrays in JavaScript can be used to store ___.",
@@ -47,27 +50,46 @@ var index = 0;
 function displayQuestion() {
   //grab question from questions variable and make <li>
   // answer question and save the click with event.targe
-  var currentQuestion = questions[index];
+  currentQuestion = questions[index];
   var li = 0;
   questionDisplay.textContent = currentQuestion.question;
   for (i = 1; i < 5; i++) {
     option[li].textContent = currentQuestion["option" + i];
     li++;
   }
+  return currentQuestion;
 }
 
 function answerQuestion(event) {
   event.stopPropagation();
   console.log("you clicked an option");
   //click 1 of the 4 answers and save this as the answer
-  var answer = event.target;
+  answer = event.target;
+  //check if correct
+
+  checkAnswer();
+
   //goes to the next question
   index++;
   if (index >= questions.length) {
     return;
   }
   displayQuestion();
-  return answer;
+}
+
+function checkAnswer() {
+  if (answer.textContent !== currentQuestion[currentQuestion.correct]) {
+    console.log("wrong");
+    // take 5 seconds from time
+    // add a message on the screen
+    message.textContent = "Incorrect!";
+    message.setAttribute("style", "color:red");
+  } else {
+    console.log("that's right!!");
+    // add a message on the screen
+    message.textContent = "Correct!";
+    message.setAttribute("style", "color:green");
+  }
 }
 
 //USER INTERACTIONS
